9d851915716436fcb147690d9405e56fa883e49b
superdaobo <yangdaobovip@163.com>
2023-07-15 16:34:38 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,13 +1,13 @@
 @echo off
-et JMETER_HOME=%cd%\apache-jmeter-5.5
+set JMETER_HOME=%cd%\apache-jmeter-5.5
 
 echo Starting JMeter...
 echo JMETER_HOME=%JMETER_HOME%
 echo.
 
-tart /B %JMETER_HOME%\bin\jmeter.bat -t %JMETER_HOME%\lcyz.jmx -Jjmeter.ave.aveervice.output_format=cv
+start /B %JMETER_HOME%\bin\jmeter.bat -t %JMETER_HOME%\lcyz.jmx -Jjmeter.save.saveservice.output_format=csv
 
-echo JMeter ha tarted.
+echo JMeter has started.
 echo.
 
-paue
+pause
